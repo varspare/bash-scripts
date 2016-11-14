4efe6bad1a7d3217eb9adfe26a54a4cb9c68a4cf
Mark Davison <varspare@users.noreply.github.com>
2016-11-14 20:48:33 +0000

    Create terminal_colour_tester.sh

---
+++ terminal_colour_tester.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo -e "Below is a list of colours to demonstrate the options available with bash."
+# Black 0;30
+echo -e "\033[30mThis is Black\033[0m"
+# Dark Gray 1;30
+echo -e "\033[30mThis is Dark Grey\033[1m"
+# Light Red 1;31
+echo -e "\033[31mThis is Light Red\033[0m"
+# Red 0;31
+echo -e "\033[31mThis is Red\033[1m"
+# Light Green 1;32
+echo -e "\033[32mThis is Light Green\033[0m"
+# Green 0;32
+echo -e "\033[32mThis is Green\033[1m"
+# Light Yellow 1;33
+echo -e "\033[33mThis is Light Yellow\033[0m"
+# Yellow 0;33
+echo -e "\033[33mThis is Yellow\033[1m"
+# Light Blue 1;34
+echo -e "\033[34mThis is Light Blue\033[0m"
+# Blue 0;34
+echo -e "\033[34mThis is Blue\033[1m"
+# Light Purple 1;35
+echo -e "\033[35mThis is Light Purple\033[0m"
+# Purple 0;35
+echo -e "\033[35mThis is Purple\033[1m"
+# Light Cyan 1;36
+echo -e "\033[36mThis is Light Cyan\033[0m"
+# Cyan 0;36
+echo -e "\033[36mThis is Cyan\033[1m"
+# White 1;37
+echo -e "\033[37mThis is White\033[0m"
+# Light Gray 0;37
+echo -e "\033[37mThis is Light Grey\033[1m"
+# default colour of console 0:0
+echo -e "\033[0mThis should be your default console colour\033[0m"
