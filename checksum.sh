b6148cd2e3bbc0ea6396a85fe631b1679b9359d2
Mark Davison <mark.davison@hivehome.com>
2018-08-18 12:07:38 +0100

    Create checksum.sh

---
+++ checksum.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+##############################
+#
+# Author Mark Davison
+# Last revision 2018/08/10
+#
+# run command first parameter is the filepath
+#
+# e.g ./checksum.sh /opt/myfiles/myfile.txt
+#
+#############################'
+file="${1}"
+cmd=''
+check=''
+lencheck=0
+stype=''
+sum=''
+if [[ ! -f "${file}" ]]
+then
+  printf "Error: Unable to reach file, please check the path\n"
+  exit 1
+fi
+
+printf "Enter the file's checksum: "
+read check
+
+#get length of check
+lencheck="$(echo $check | wc -c)"
+if [[ "${lencheck}" -eq 65 ]];then
+  stype='sha256'
+elif [[ "${lencheck}" -eq 41 ]];then
+  stype='sha1'
+elif [[ "${lencheck}" -eq 33 ]];then
+  stype='md5'
+elif [[ "${lencheck}" -eq 57 ]];then
+  stype='sha224'
+elif [[ "${lencheck}" -eq 97 ]];then
+  stype='sha384'
+elif [[ "${lencheck}" -eq 129 ]];then
+  stype='sha512'
+elif [[ "${lencheck}" -eq 11 ]];then
+  stype='ck' #CRC
+elif [[ "${lencheck}" -eq 6 ]];then
+  stype='' #BSD sum command
+else
+  printf "Unable to determine hash type\nPlease enter hash type manually: "
+  read stype
+fi
+
+cmd=$(which "${stype}sum")
+
+if [[ -z "${cmd}" ]]
+then
+  printf "Error: command not found for hash type: ${stype}\n"
+  exit 1
+fi
+
+sum="$($cmd $file| cut -d' ' -f1)"
+
+if [[ "$sum" == "${check}" ]]
+  then
+    printf 'match\n'
+  else
+    printf 'fail\n'
+fi
+
+exit "${?}"
